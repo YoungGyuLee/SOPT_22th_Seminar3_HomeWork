89744f82739ec21eef2379e8d9d02a67cdedba2f
Woo <jhsw0375@gmail.com>
2018-06-03 12:44:55 +0900

    7차 세미나 -> 3차 세미나 내용에서 애니메이션 기능 추가(카카오톡과 비슷한 기능) 밀어서 DELETE와 EDIT 나오는 애니메이션, 플로팅 버튼 클릭시 이벤트

--- app/src/main/java/woo/sopt22/seminar3/MainActivity.kt
+++ app/src/main/java/woo/sopt22/seminar3/MainActivity.kt
@@ -2,10 +2,15 @@ package woo.sopt22.seminar3
 
 import android.content.Context
 import android.content.Intent
+import android.graphics.Canvas
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.support.v7.widget.helper.ItemTouchHelper
 import android.view.View
+import android.view.animation.AnimationUtils
+import android.widget.RelativeLayout
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.kakao_item.*
 import woo.sopt22.seminar3.Adapter.KaKaoAdapter
@@ -35,13 +40,20 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     // 3가지 방법이 있음
     var context : Context = this
 
+    var isDisplayButtons : Boolean = false
+
     lateinit var kakaoAdapter: KaKaoAdapter
+    lateinit var swipeController: SwipeController
+    lateinit var itemTouchListener : ItemTouchHelper
     /*FIXME
     * lateinit : 초기화를 지금하지 않고 나중에 초기화를 진행할 수 있도록 하는 방법
     * */
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
 
         kakaoItems = ArrayList()
         kakaoItems.add(KaKaoData(R.drawable.pic1,"09의 바나나 안드로이드","낰낰","오후 4:07"))
@@ -63,5 +75,114 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         main_recyclerview.layoutManager = LinearLayoutManager(this)
         main_recyclerview.adapter = kakaoAdapter
 
+        swipeController = SwipeController() // 초기화
+        itemTouchListener = ItemTouchHelper(swipeController)
+        itemTouchListener.attachToRecyclerView(main_recyclerview)
+
+
+        // 리사이클러뷰 아이템에 그림을 그려줄 것
+
+        main_recyclerview.addItemDecoration(object : RecyclerView.ItemDecoration(){
+
+            override fun onDraw(c: Canvas?, parent: RecyclerView?, state: RecyclerView.State?) {
+                swipeController.onDraw(c!!)
+            }
+        })
+
+        main_float_add.setOnClickListener {
+            clickFloat()
+        }
+        main_float_category1.setOnClickListener {
+            // 여기서 그냥 이벤트 넣어주면 된다.
+        }
+
+    }
+
+    // 버튼을 누를때마다 호출되는 함수
+
+    fun clickFloat(){
+        if(!isDisplayButtons){ // false일 경우
+            // 평소 상태. 버튼이 보여지지 않고 있는 상태
+
+            isDisplayButtons = true
+
+            main_wrapper_layout.visibility = View.VISIBLE
+            main_wrapper_layout.setOnClickListener{
+                clickFloat()
+            }
+
+            val animation = AnimationUtils.loadAnimation(this, R.anim.float_main_show)
+            main_float_add.setBackgroundResource(R.drawable.pic4)
+            // 아무것도 누르지 않은 상태에서 add를 눌렀을 때 보여지는 액션이 나오면서
+            // 그 때 버튼의 background가 취소 버튼으로 보여지는 액션을 취한다.
+            main_float_add.startAnimation(animation) // 애니메이션 등록
+
+            val layoutParam1 = main_float_category1.layoutParams as RelativeLayout.LayoutParams // Activity에서 xml을 다루어야 할 때 layoutParam을 자주 사용한다.
+            // 위는 부모 레이아웃의 layoutParam이라는 객체를 통해서 접근한다.
+            layoutParam1.bottomMargin +=(main_float_category1.height * 1.2).toInt()
+
+
+
+            val showC1 =  AnimationUtils.loadAnimation(this, R.anim.float_button1_show)
+            main_float_category1.layoutParams = layoutParam1
+            main_float_category1.startAnimation(showC1)
+            main_float_category1.isClickable = true // 클릭할 수 있게끔
+
+
+            val layoutParam2 = main_float_category2.layoutParams as RelativeLayout.LayoutParams // Activity에서 xml을 다루어야 할 때 layoutParam을 자주 사용한다.
+            // 위는 부모 레이아웃의 layoutParam이라는 객체를 통해서 접근한다.
+            layoutParam2.bottomMargin +=(main_float_category2.height * 2.4).toInt()
+
+
+
+            val showC2 =  AnimationUtils.loadAnimation(this, R.anim.float_button2_show)
+            main_float_category2.layoutParams = layoutParam2
+            main_float_category2.startAnimation(showC2)
+            main_float_category2.isClickable = true // 클릭할 수 있게끔
+
+
+
+
+
+        } else{ // true일 경우
+            // 버튼이 보여지고 있는 상태
+
+            isDisplayButtons = false
+
+            main_wrapper_layout.visibility = View.INVISIBLE
+
+            val animation = AnimationUtils.loadAnimation(this, R.anim.float_main_hide)
+            main_float_add.setBackgroundResource(R.drawable.pic1)
+            // 아무것도 누르지 않은 상태에서 add를 눌렀을 때 보여지는 액션이 나오면서
+            // 그 때 버튼의 background가 취소 버튼으로 보여지는 액션을 취한다.
+            main_float_add.startAnimation(animation) // 애니메이션 등록
+
+            val layoutParam1 = main_float_category1.layoutParams as RelativeLayout.LayoutParams // Activity에서 xml을 다루어야 할 때 layoutParam을 자주 사용한다.
+            // 위는 부모 레이아웃의 layoutParam이라는 객체를 통해서 접근한다.
+            layoutParam1.bottomMargin -=(main_float_category1.height * 1.2).toInt()
+
+
+
+            val showC1 =  AnimationUtils.loadAnimation(this, R.anim.float_button1_hide)
+            main_float_category1.layoutParams = layoutParam1
+            main_float_category1.startAnimation(showC1)
+            main_float_category1.isClickable = false // 클릭할 수 있게끔
+
+
+            val layoutParam2 = main_float_category2.layoutParams as RelativeLayout.LayoutParams // Activity에서 xml을 다루어야 할 때 layoutParam을 자주 사용한다.
+            // 위는 부모 레이아웃의 layoutParam이라는 객체를 통해서 접근한다.
+            layoutParam2.bottomMargin -=(main_float_category2.height * 2.4).toInt()
+
+
+
+            val showC2 =  AnimationUtils.loadAnimation(this, R.anim.float_button2_hide)
+            main_float_category2.layoutParams = layoutParam2
+            main_float_category2.startAnimation(showC2)
+            main_float_category2.isClickable = false // 클릭할 수 있게끔
+
+
+
+
+        }
     }
 }
